77b520856acfc6348c3912d5737271b7291b6073
babyfish-ct <babyfish.ct@gmail.com>
2026-02-04 19:06:05 +0800

    Export API of core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,1 +1,5 @@
+export type { Model } from "./schema/model";
+export type { PropData } from "./schema/prop";
+export { model } from "./schema/model";
+export { prop } from "./schema/prop";
 export { dsl } from "./dsl";
